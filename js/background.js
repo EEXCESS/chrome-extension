38f418d46743d16b0568fea8d5b72a43e63c263a
Jörg Schlötterer <joerg.schloetterer@uni-passau.de>
2015-10-06 19:13:15 +0200

    less permissions required + minor

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ require(['./common'], function(common) {
     require(['c4/APIconnector'], function(APIconnector) {
 //        APIconnector.init({base_url:'http://eexcess-dev.joanneum.at/eexcess-privacy-proxy-1.0-SNAPSHOT/api/v1/'});
 //        APIconnector.init({base_url:'http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/'});
-        APIconnector.init({base_url:'http://eexcess-demo.know-center.tugraz.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/'});
+//        APIconnector.init({base_url:'http://eexcess-demo.know-center.tugraz.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/'});
         var msgAllTabs = function(msg) {
             chrome.tabs.query({}, function(tabs) {
                 for (var i = 0, len = tabs.length; i < len; i++) {
@@ -18,12 +18,12 @@ require(['./common'], function(common) {
                     case 'triggerQuery':
                         var profile = msg.data;
                         // TODO: adapt origin
-//                        profile.origin = {
-//                            userID:"1",
-//                            clientType:"chrome-extension",
-//                            clientVersion:"42",
-//                            module:"hilde"
-//                        };
+                        profile.origin = {
+                            userID:"1",
+                            clientType:"chrome-extension",
+                            clientVersion:"0.52",
+                            module:"unknown"
+                        };
                         APIconnector.query(profile, sendResponse);
                         return true;
                         break;
