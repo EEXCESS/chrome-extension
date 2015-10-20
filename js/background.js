6fe114aefc9d213d3d74469a6ab9c1d6beaa9b20
Jörg Schlötterer <joerg.schloetterer@uni-passau.de>
2015-10-20 10:34:36 +0200

    initialize APIconnector with origin

--- js/background.js
+++ js/background.js
@@ -1,10 +1,16 @@
-require(['./common'], function(common) {
-    require(['c4/APIconnector'], function(APIconnector) {
+require(['./common'], function (common) {
+    require(['c4/APIconnector'], function (APIconnector) {
 //        APIconnector.init({base_url:'http://eexcess-dev.joanneum.at/eexcess-privacy-proxy-1.0-SNAPSHOT/api/v1/'});
 //        APIconnector.init({base_url:'http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/'});
 //        APIconnector.init({base_url:'http://eexcess-demo.know-center.tugraz.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/'});
-        var msgAllTabs = function(msg) {
-            chrome.tabs.query({}, function(tabs) {
+
+        APIconnector.init({origin: {
+                userID: "1",
+                clientType: "chrome-extension",
+                clientVersion:"0.53"
+            }});
+        var msgAllTabs = function (msg) {
+            chrome.tabs.query({}, function (tabs) {
                 for (var i = 0, len = tabs.length; i < len; i++) {
                     chrome.tabs.sendMessage(tabs[i].id, msg);
                 }
@@ -12,17 +18,14 @@ require(['./common'], function(common) {
         };
 
 
-        chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+        chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
             if (typeof msg.method !== 'undefined') {
                 switch (msg.method) {
                     case 'triggerQuery':
                         var profile = msg.data;
                         // TODO: adapt origin
                         profile.origin = {
-                            userID:"1",
-                            clientType:"chrome-extension",
-                            clientVersion:"0.52",
-                            module:"unknown"
+                            module: "unknown"
                         };
                         APIconnector.query(profile, sendResponse);
                         return true;
