12f83a2b34a201ef16f952a3662d2ad47441a9d0
Thomas Cerqueus <thomas.cerqueus@ucd.ie>
2015-10-26 16:59:18 +0100

    Merge

--- js/common.js
+++ js/common.js
@@ -10,14 +10,10 @@ require.config({
     bootstrap: "bootstrap/dist/js/bootstrap",
     "jquery-ui": "jquery-ui/jquery-ui",
     graph: "graph/lib/graph",
-<<<<<<< HEAD
     "tag-it": "tag-it/js/tag-it", 
     up: "../user_profile"
-=======
     "tag-it": "tag-it/js/tag-it",
     peas_indist: "peas/peas_indist",
-    peas_adapt: "peas/peas_adapt"
->>>>>>> 746a82b75f341260b03b7d2b0a5cf5a3ec5078d1
   },
   packages: [
 
